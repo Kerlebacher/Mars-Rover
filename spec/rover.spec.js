6c68aa8a8bd8d6e9688d2d37551f24ac10ef2ac2
Kerlebacher <154646865+Kerlebacher@users.noreply.github.com>
2024-03-07 16:29:46 +0000

    Rover outline done

---
+++ spec/rover.spec.js
@@ -0,0 +1,36 @@
+const Rover = require('../rover.js');
+const Message = require('../message.js');
+const Command = require('../command.js');
+
+describe("Rover class", function() {
+
+  // spec 7
+  it("constructor sets position and default values for mode and generatorWatts", function(){
+    expect().toBe()
+  });
+   // spec 8
+   it("response returned by receiveMessage contains the name of the message", function(){
+    expect().toBe()
+  });
+   // spec 9
+   it("response returned by receiveMessage includes two results if two commands are sent in the message", function(){
+    expect().toBe()
+  });
+   // spec 10
+   it("responds correctly to the status check command", function(){
+    expect().toBe()
+  });
+   // spec 11
+   it("responds correctly to the mode change command", function(){
+    expect().toBe()
+  });
+   // spec 12
+   it("responds with a false completed value when attempting to move in LOW_POWER mode", function(){
+    expect().toBe()
+  });
+   // spec 13
+   it("responds with the position for the move command", function(){
+    expect().toBe()
+  });
+
+});
