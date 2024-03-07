d14779035f138dec55c97ba147957a1811f95c11
Kerlebacher <154646865+Kerlebacher@users.noreply.github.com>
2024-03-07 04:32:50 +0000

    updating

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -5,16 +5,16 @@ const Command = require('../command.js');
 
 describe("Command class", function() {
 //spec 1
-  // it("throws error if command type is NOT passed into constructor as the first parameter", function() {
-  //   expect( function() { new Command();}).toThrow(new Error('Command type required.'));
-  // });
+  it("throws error if command type is NOT passed into constructor as the first parameter", function() {
+    expect( function() { new Command();}).toThrow(new Error('Command type required.'));
+  });
 //spec 2
   it("constructor sets command type", function() {
-    expect(commandType).toBe(String)
+    expect(new Command('SOME_STRING').commandType).toBe('SOME_STRING')
   });
 
 //spec 3
 it("constructor sets a value passed in as the 2nd argument", function() {
-  expect(commandType).toBe(String)
+  expect(new Command('SOME_STRING', 'SOME_VALUE').value).toBe('SOME_VALUE')
 });
 });
